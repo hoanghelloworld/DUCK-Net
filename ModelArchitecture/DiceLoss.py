d49ade148f27fa39260af05c1468fe2bfd430345
Nguyễn Huy Hoàng <115699781+hoanghelloworld@users.noreply.github.com>
2025-05-19 04:03:32 +0000

    Refactor image and mask loading functions for improved preprocessing; update model parameters for better compatibility and performance.

--- ModelArchitecture/DiceLoss.py
+++ ModelArchitecture/DiceLoss.py
@@ -1,15 +1,14 @@
-import keras.backend as K
 import tensorflow as tf
 
 
 def dice_metric_loss(ground_truth, predictions, smooth=1e-6):
-    ground_truth = K.cast(ground_truth, tf.float32)
-    predictions = K.cast(predictions, tf.float32)
-    ground_truth = K.flatten(ground_truth)
-    predictions = K.flatten(predictions)
-    intersection = K.sum(predictions * ground_truth)
-    union = K.sum(predictions) + K.sum(ground_truth)
+    ground_truth = tf.cast(ground_truth, tf.float32)
+    predictions = tf.cast(predictions, tf.float32)
+    ground_truth = tf.reshape(ground_truth, [-1])
+    predictions = tf.reshape(predictions, [-1])
 
-    dice = (2. * intersection + smooth) / (union + smooth)
+    intersection = tf.reduce_sum(ground_truth * predictions)
+    union = tf.reduce_sum(ground_truth) + tf.reduce_sum(predictions)
 
+    dice = (2.0 * intersection + smooth) / (union + smooth)
     return 1 - dice
